0514dd40ffd6dadd87e4bc0dcffe8902f3737862
elibahlcke <elianabahlcke@gmail.com>
2021-08-20 01:15:34 -0300

    added some styles and started editting header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/header.scss'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1 className="title">Todo List</h1>
+            
+        </div>
+    )
+}
+
+export default Header
